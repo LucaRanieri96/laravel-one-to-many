72e30b390f808a1c06a51c24a4af1ede2ed9f27f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-07 14:46:56 +0200

    Add relationship oneToMany to classes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'slug', 'repoUrl', 'startingDate'];
+    protected $fillable = ['name', 'slug', 'repoUrl', 'startingDate', 'type_id'];
+
+    public function type(): BelongsTo 
+    {
+        return $this->BelongsTo(Type::class);
+    }
 
     public static function generateSlug($name)
     {
